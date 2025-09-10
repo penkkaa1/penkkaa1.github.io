448089a1cc1973a8fcf2224e0f32bc67cb3889ac
penkkaa1 <aaron.penkkala@aalto.fi>
2025-09-10 15:15:03 +0300

    Initial layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import photo from './assets/photo.jpg';
+import linkedin from './assets/InBug-White.png';
+import github from './assets/github-mark-white.png';
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,14 +11,22 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+        <img src={photo} className="hoverShadow" alt="Photo of me" style={{ height: "30vh", padding: "1.5em", borderRadius: "50%" }} />
+      </div>
+      <div style={{ display: "flex", justifyContent: "center", alignItems: "center", gap: "10px", margin:"0px 0px 40px" }}>
+        <h1 style={{ margin: "0 0", paddingRight: "5px", display: "flex", alignItems: "center" }}>
+          Aaron Penkkala
+        </h1>
+        <a href="https://github.com/penkkaa1" className="hoverShadow-sm" style={{ display: "flex", alignItems: "center" }}>
+          <img src={github} alt="GitHub logo" style={{ height: "2.5em" }} />
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="https://fi.linkedin.com/in/aaron-penkkala-343951199" className='hoverShadow-sm' style={{ display: "flex", alignItems: "center" }}>
+          <img src={linkedin} alt="LinkedIn logo" style={{ height: "2.5em" }} />
         </a>
       </div>
-      <h1>Welcome to my Website!</h1>
+      <h2>4th Year Computer Science Student at Aalto University</h2>
+      <h2>Majoring in Secure Systems</h2>
+
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           Count is {count}
