30d5966d8c1f1ed338efe0190edbadf536cfd386
penkkaa1 <aaron.penkkala@aalto.fi>
2025-09-10 15:50:27 +0300

    Make links open new website, add dynamic fontsize

--- src/App.tsx
+++ src/App.tsx
@@ -13,14 +13,14 @@ function App() {
       <div>
         <img src={photo} className="hoverShadow" alt="Photo of me" style={{ height: "30vh", padding: "1.5em", borderRadius: "50%" }} />
       </div>
-      <div style={{ display: "flex", justifyContent: "center", alignItems: "center", gap: "10px", margin:"0px 0px 40px" }}>
+      <div style={{ display: "flex", justifyContent: "center", alignItems: "center", gap: "10px", margin: "0px 0px 40px" }}>
         <h1 style={{ margin: "0 0", paddingRight: "5px", display: "flex", alignItems: "center" }}>
           Aaron Penkkala
         </h1>
-        <a href="https://github.com/penkkaa1" className="hoverShadow-sm" style={{ display: "flex", alignItems: "center" }}>
+        <a href="https://github.com/penkkaa1" className="hoverShadow-sm" target="_blank" style={{ display: "flex", alignItems: "center" }}>
           <img src={github} alt="GitHub logo" style={{ height: "2.5em" }} />
         </a>
-        <a href="https://fi.linkedin.com/in/aaron-penkkala-343951199" className='hoverShadow-sm' style={{ display: "flex", alignItems: "center" }}>
+        <a href="https://fi.linkedin.com/in/aaron-penkkala-343951199" className='hoverShadow-sm' target="_blank" style={{ display: "flex", alignItems: "center" }}>
           <img src={linkedin} alt="LinkedIn logo" style={{ height: "2.5em" }} />
         </a>
       </div>
